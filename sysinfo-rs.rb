be3a393ae80a317158f509d6873ef34c7c7c536b
ChrisKp1710 <chriskp1710@gmail.com>
2025-11-21 02:27:03 +0100

    Fix sysinfo-rs formula: use commit hash for stable download

--- sysinfo-rs.rb
+++ sysinfo-rs.rb
@@ -1,8 +1,9 @@
 class SysinfoRs < Formula
   desc "🌈 The most beautiful system information tool for macOS"
   homepage "https://github.com/ChrisKp1710/sysinfo-rs"
-  url "https://github.com/ChrisKp1710/sysinfo-rs/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
+  url "https://github.com/ChrisKp1710/sysinfo-rs/archive/a7d83cf07c944a840c09282864ce9dc433727ecc.tar.gz"
+  version "0.1.0"
+  sha256 "9cad7df070f0e1c33a419eaa77f3b1b8b5cc17ae224b3fe11f36182a11d7e4d0"
   license "MIT"
 
   depends_on "rust" => :build
